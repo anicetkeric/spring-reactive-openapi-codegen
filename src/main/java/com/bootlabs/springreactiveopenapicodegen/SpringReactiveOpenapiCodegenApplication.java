488e43c11fd1cc07f38b74442b372b66d8cbdaef
aek <anicetkeric@outlook.com>
2022-12-10 20:59:06 +0100

    init architecture

---
+++ src/main/java/com/bootlabs/springreactiveopenapicodegen/SpringReactiveOpenapiCodegenApplication.java
@@ -0,0 +1,13 @@
+package com.bootlabs.springreactiveopenapicodegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveOpenapiCodegenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveOpenapiCodegenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootlabs/springreactiveopenapicodegen/SpringReactiveOpenapiCodegenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootlabs.springreactiveopenapicodegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveOpenapiCodegenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
